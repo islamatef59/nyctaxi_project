c6e127f6c1ac4394d79b30724a8c1e4e2ca10718
islamatef59 <islamatef59@gmail.com>
2025-12-12 15:48:14 +0000

    print path

--- transformations/notebooks/00_landing/ingest_lookup.py
+++ transformations/notebooks/00_landing/ingest_lookup.py
@@ -9,6 +9,7 @@ project_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))
 
 if project_root not in sys.path:
     sys.path.append(project_root)
+print(project_root)
 
 
 # COMMAND ----------
